0529440be992e8dcb692419a4cafe92ac5f4e3a0
swodesh-2001 <70265297+swodesh-2001@users.noreply.github.com>
2021-07-06 19:51:10 +0545

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,68 +1,83 @@
 #include "func.hpp"
 #include "components.hpp"
+ 
+const int menu_size = 2;
+const int rectangle_size = 500;
+const int square_size = 500;
 
-vector<menu> menu_points{menu("rectangle"),menu("square"),menu("pen")};
-vector<rectangle> rectangle_points;
-vector<square> square_points;
-vector<pen> pen_points;
+
+int rectangle_temp =  0;
+int square_temp =  0;
+
+
+
+menu menu_points[menu_size] = {menu("rectangle"),menu("square")};
+rectangle rectangle_points[rectangle_size]{};
+
+square square_points[square_size]{};
+
 
 
 void mouse_func(int button , int state ,  int x , int y){
 
-         for(auto &i : rectangle_points){
-        if(i.hovering_status(a,b)){
-            if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN ){
-               i.change_flag(true);
+    for(int i = 0 ; i < rectangle_size ; i++){
+        if(rectangle_points[i].hovering_status(a,b)){
+              if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN ){
+               rectangle_points[i].change_flag(true);
         }
         }
         }
 
-
-    for(auto &i : square_points){
-        if(i.hovering_status(a,b)){
-            if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN ){
-               i.change_flag(true);
+    for(int i = 0 ; i < square_size ; i++){
+        if(square_points[i].hovering_status(a,b)){
+              if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN ){
+               square_points[i].change_flag(true);
         }
         }
         }
 
     int temp;
-    for (auto &i : menu_points){
-        if(i.hovering_status(a,b)){
-            if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN ){
-                 temp = i.get_id();
-                 i.change_flag(true);
-                 for (auto &i : menu_points){if(i.get_id() != temp){i.change_flag(false);}}
-            }
-
+    for(int i = 0 ; i < menu_size ; i++){
+        if(menu_points[i].hovering_status(a,b)){
+              if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN ){
+               temp = menu_points[i].get_id();
+                menu_points[i].change_flag(true);
+                for(int i = 0 ; i < menu_size ; i++){
+                    if(menu_points[i].get_id() != temp){menu_points[i].change_flag(false);
+                }
         }
         }
+        }
 
 
-    if(button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN ){
-        for(auto &i : pen_points){i.change_flag(false);}
-        for(auto &i : menu_points){
-                if(i.get_flag()){
-                    if(i.get_name() == "rectangle"){rectangle_points.push_back(rectangle(x,y));}
-                    if(i.get_name() == "square"){square_points.push_back(square(x,y));}
-                    if(i.get_name() == "pen"){pen_points.push_back(pen());}
-                    i.change_flag(false);
+
+   if(button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN ){
+        for(int i = 0 ; i < menu_size ; i++){
+                if(menu_points[i].get_flag()){
+                    if(menu_points[i].get_name() == "rectangle"){rectangle_points[rectangle_temp] = rectangle(x,y); rectangle_temp++;}
+                    if(menu_points[i].get_name() == "square"){square_points[square_temp] = square(x,y); square_temp++;}
+                   menu_points[i].change_flag(false);
                 }
         }
 
-        for(auto &i : rectangle_points){i.change_flag(false);}
-        for(auto &i : square_points){i.change_flag(false);}
+
+        for(int i = 0 ; i < rectangle_size ; i++){rectangle_points[i].change_flag(false);}
+        for(int i = 0 ; i < square_size ; i++){square_points[i].change_flag(false);}
         }
 
 
-}
+
+
+
+}}
 
 
 void motion_mouse_func( int x , int y){
 
-    for(auto &i : rectangle_points){if(i.get_flag()){i.change_pos(a,b);}}
-    for(auto &i : square_points){if(i.get_flag()){i.change_pos(a,b);}}
-    for(auto &i : pen_points){ if(i.get_flag()){i.x_points.push_back(a);i.y_points.push_back(b);}}
+
+        for(int i = 0 ; i < rectangle_size ; i++){if(rectangle_points[i].get_flag()){rectangle_points[i].change_pos(a,b);}}
+        for(int i = 0 ; i < square_size ; i++){if(square_points[i].get_flag()){square_points[i].change_pos(a,b);}}
+
 
     glutMouseFunc(mouse_func);
     a = x;
@@ -99,11 +114,11 @@ glLoadIdentity();
 glTranslatef(-250,250,0);
 glRotatef(180,1,0,0);
 
+for (int i = 0 ; i < menu_size ; i++){menu_points[i].create();menu_points[i].if_hovering(a,b);menu_points[i].draw(a,b);}
+for (int i = 0 ; i < rectangle_size ; i++){rectangle_points[i].create();rectangle_points[i].if_hovering(a,b);}
+for (int i = 0 ; i < square_size ; i++){square_points[i].create();square_points[i].if_hovering(a,b);}
 
-for(auto &i : menu_points){i.create();i.if_hovering(a,b);i.draw(a,b);}
-for(auto &i : rectangle_points){i.create();i.if_hovering(a,b);}
-for(auto &i : square_points){i.create();i.if_hovering(a,b);}
-for(auto &i : pen_points){i.create();}
+
 
 
 glutPassiveMotionFunc(motion_mouse_func);
@@ -112,4 +127,3 @@ glutSwapBuffers();
 
 
 }
-
