43b8ab933b7ce7953aedac8b3cffc908da763902
swodesh-2001 <70265297+swodesh-2001@users.noreply.github.com>
2021-07-21 23:56:49 +0545

    Update func.hpp

--- func.hpp
+++ func.hpp
@@ -3,7 +3,7 @@
 #include <GL/glut.h>
 #include <math.h>
 #include <iostream>
-#define PI 3.14
+#define PI 3.14159
 #include <vector>
 #include <string>
 using namespace std;
@@ -21,16 +21,9 @@ glClearColor(0,0,0,1.0);
 }
 
 
-
-
-
 int a,b;
 
-
-
-
 void create_square(float x,float y ,int w = 20, int h = 20 , float r=1 ,float g = 1 ,float b = 1){
-    if(h==0){h=1;}
 glBegin(GL_QUADS);
 float xl,xh,yl(y),yh;
 yh = yl + h;
@@ -45,19 +38,32 @@ glEnd();
 }
 
 
-
-
-
-
-void create_square(int x , int y,int size=1){
-create_square(x,y,size*40,size*5);
-create_square(x+size*15,y+size*16,size*10,size*5);
+void create_triangle(float x,float y ,int w = 20, int h = 20 , float r=1 ,float g = 1 ,float b = 1){
+glBegin(GL_TRIANGLES);
+float xl,xh,xh1,yl(y),yh;
+yh = yl + h;
+xl=x;
+xh = xl + w;
+xh1 = xl - w;
+glColor3f(r,g,b);
+glVertex2f(xl,yl);
+glVertex2f(xh,yh);
+glVertex2f(xh1,yh);
+glEnd();
 }
 
-void create_rectangle(int x , int y,int size=1){
-    create_square(x , y , size*50 , size*15 ,1,1,1);
+
+void create_square(int x , int y,float size=1.0,float r=1,float g=1, float b=1){
+create_square(x,y,size*50,size*50,r,g,b);
 }
 
+void create_rectangle(int x , int y,float size=1,float r=1,float g=1,float b=1){
+    create_square(x , y , size*50 , size*20 ,r,g,b);
+}
+
+void create_triangle(int x , int y,float size=1.0,float r=1,float g=1, float b=1){
+create_triangle(x,y,size*20,size*40,r,g,b);
+}
 
 
 void create_line(float x1,float y1 ,float x2,float y2){
@@ -73,10 +79,18 @@ glEnd();
 
 
 
+
+void create_circle(int x , int y , float radius, float r , float g , float b,int line_width = 5){
+for(float i=0 ; i<360 ; i+=0.3){
+        create_square(x + radius * cos(i * PI/180),y + radius * sin(i * PI/180),line_width,line_width,r,g,b);
+
+}
+}
+
+
 void reshape(int w,int h){
-//viewport
+
 glViewport(0,0,w,h);
-//projection
 
 glMatrixMode(GL_PROJECTION);
 glLoadIdentity();
