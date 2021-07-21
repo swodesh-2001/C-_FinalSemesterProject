980bc6296d58106a0b0ad297f452446d7aabbb3b
swodesh-2001 <70265297+swodesh-2001@users.noreply.github.com>
2021-07-21 23:56:18 +0545

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,24 +2,32 @@
 #include "components.hpp"
 
 int width =1000 , height = 700;
+float rr[6]={0.0,0.0,0.0,0.0,0.0},gr[6]={0.9,0.9,0.9,0.9,0.9},br[6]={0.0,0.0,0.0,0.0,0.0};
 
-
-
-const int menu_size = 2;
+const int menu_size = 4;
 const int rectangle_size = 500;
 const int square_size = 500;
+const int triangle_size = 500;
+const int circle_size = 500;
 
+float red=1;
+float green=1;
+float blue=1;
 
 int rectangle_temp =  0;
 int square_temp =  0;
+int triangle_temp =  0;
+int circle_temp =  0;
 
 
-
-menu menu_points[menu_size] = {menu("square"),menu("rectangle")};
+menu menu_points[menu_size] = {menu("square"),menu("rectangle"),menu("triangle"),menu("circle")};
 rectangle rectangle_points[rectangle_size]{};
 
 square square_points[square_size]{};
 
+triangle triangle_points[triangle_size]{};
+
+circle circle_points[circle_size]{};
 
 
 void mouse_func(int button , int state ,  int x , int y){
@@ -47,6 +55,7 @@ void mouse_func(int button , int state ,  int x , int y){
         }
         }
 
+
     for(int i = 0 ; i < square_size ; i++){
         if(square_points[i].hovering_status(a,b)){
               if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN ){
@@ -55,6 +64,21 @@ void mouse_func(int button , int state ,  int x , int y){
         }
         }
 
+    for(int i = 0 ; i < triangle_size ; i++){
+        if(triangle_points[i].hovering_status(a,b)){
+              if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN ){
+               triangle_points[i].change_flag(true);
+        }
+        }
+        }
+
+    for(int i = 0 ; i < circle_size ; i++){
+        if(circle_points[i].hovering_status(a,b)){
+              if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN ){
+               circle_points[i].change_flag(true);
+        }
+        }
+        }
 
    if(button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN ){
         for(int i = 0 ; i < menu_size ; i++){
@@ -71,17 +95,42 @@ void mouse_func(int button , int state ,  int x , int y){
                         square_temp++;
                         }
 
+                    if(menu_points[i].get_name() == "triangle"){
+                        triangle_points[triangle_temp] = triangle(x,y);
+                        triangle_points[triangle_temp].change_create_flag();
+                        triangle_temp++;
+                        }
+
+                    if(menu_points[i].get_name() == "circle"){
+                        circle_points[circle_temp] = circle(x,y);
+                        circle_points[circle_temp].change_create_flag();
+                        circle_temp++;
+                        }
+
                    menu_points[i].change_flag(false);
                 }
         }
 
 
         for(int i = 0 ; i < rectangle_size ; i++){rectangle_points[i].change_flag(false);}
+        for(int i = 0 ; i < triangle_size ; i++){triangle_points[i].change_flag(false);}
         for(int i = 0 ; i < square_size ; i++){square_points[i].change_flag(false);}
+        for(int i = 0 ; i < circle_size ; i++){circle_points[i].change_flag(false);}
         }
 
 
+    for (int i=0;i<5;i++)
+    {
+        if (x>=750+50*i && x<=800+50*i && y>=0 && y<=50 && button==GLUT_LEFT_BUTTON && state==GLUT_DOWN)
+        {
 
+        if (i==0){glClearColor(0.8,0,0,1);}
+        if (i==1){glClearColor(0.9,0.9,0,1);}
+        if (i==2){glClearColor(0.98,0.05,0.90,1);}
+        if (i==3){glClearColor(0,0,0,1);red=1;green=1;blue=1;}
+        if (i==4){glClearColor(1,1,1,1);red=0,green=0,blue=0;}
+        }
+    }
 
 
 }}
@@ -89,7 +138,17 @@ void mouse_func(int button , int state ,  int x , int y){
 
 void motion_mouse_func( int x , int y){
         for(int i = 0 ; i < rectangle_size ; i++){if(rectangle_points[i].get_flag()){rectangle_points[i].change_pos(a,b);}}
+        for(int i = 0 ; i < triangle_size ; i++){if(triangle_points[i].get_flag()){triangle_points[i].change_pos(a,b);}}
         for(int i = 0 ; i < square_size ; i++){if(square_points[i].get_flag()){square_points[i].change_pos(a,b);}}
+        for(int i = 0 ; i < circle_size ; i++){if(circle_points[i].get_flag()){circle_points[i].change_pos(a,b);}}
+
+        for (int i=0;i<5;i++)
+        {
+        if (x>=750+i*50 && x<=800+50*i && y>=0 && y<=50){rr[i]=0.0,gr[i]=1.0,br[i]=1.0;}
+        else {rr[i]=0.0,gr[i]=1.0,br[i]=0.0;}
+        }
+
+
     glutMouseFunc(mouse_func);
     a = x;
     b = y;
@@ -98,6 +157,8 @@ void motion_mouse_func( int x , int y){
 void keyboard(unsigned char c , int x , int y){
 for(int i = 0 ; i < rectangle_size ; i++){if(rectangle_points[i].get_flag()){rectangle_points[i].change_variables(c);}}
 for(int i = 0 ; i < square_size ; i++){if(square_points[i].get_flag()){square_points[i].change_variables(c);}}
+for(int i = 0 ; i < triangle_size ; i++){if(triangle_points[i].get_flag()){triangle_points[i].change_variables(c);}}
+for(int i = 0 ; i < circle_size ; i++){if(circle_points[i].get_flag()){circle_points[i].change_variables(c);}}
 }
 
 
@@ -116,20 +177,32 @@ glutMainLoop();
 }
 
 
-
-
 void display()
 {
-
 
 glClear(GL_COLOR_BUFFER_BIT);
 glLoadIdentity();
 glTranslatef(-width/2,height/2,0);
 glRotatef(180,1,0,0);
 
-for (int i = 0 ; i < menu_size ; i++){menu_points[i].create(); menu_points[i].if_hovering(a,b); menu_points[i].draw(a,b);}
-for (int i = 0 ; i < rectangle_size ; i++){rectangle_points[i].create();rectangle_points[i].if_hovering(a,b);}
-for (int i = 0 ; i < square_size ; i++){square_points[i].create();square_points[i].if_hovering(a,b);}
+for (int i=0; i<5;i++)
+{
+create_rectangle(750+50*i,0,2.5,rr[i],gr[i],br[i]);
+}
+create_square(755,6,0.8,1.0,0.0,0.0);
+create_square(805,6,0.8,1.0,1.0,0.0);
+create_square(855,6,0.8,0.99,0.1,1.0);
+create_square(905,6,0.8,0.0,0.0,0.0);
+create_square(955,6,0.8,1.0,1.0,1.0);
+
+
+
+for (int i = 0 ; i < menu_size ; i++){menu_points[i].create(red,green,blue); menu_points[i].if_hovering(a,b); menu_points[i].draw(a,b,red,green,blue);}
+
+for (int i = 0 ; i < rectangle_size ; i++){rectangle_points[i].create(red,green,blue);rectangle_points[i].if_hovering(a,b);}
+for (int i = 0 ; i < square_size ; i++){square_points[i].create(red,green,blue);square_points[i].if_hovering(a,b);}
+for (int i = 0 ; i < triangle_size ; i++){triangle_points[i].create(red,green,blue);triangle_points[i].if_hovering(a,b);}
+for (int i = 0 ; i < circle_size ; i++){circle_points[i].create(red,green,blue);circle_points[i].if_hovering(a,b);}
 
 glutKeyboardFunc(keyboard);
 glutPassiveMotionFunc(motion_mouse_func);
